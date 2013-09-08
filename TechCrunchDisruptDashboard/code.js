160421c086765bd800cc7a5245a3fcfe3adaf786
Jonah Kim <jkk.jonah@gmail.com>
2013-09-08 05:05:30 -0700

    starting to stylize the demo app

--- TechCrunchDisruptDashboard/code.js
+++ TechCrunchDisruptDashboard/code.js
@@ -1,9 +1,20 @@
 var $label, $sync, $id, myLat, myLng, myId, phoneLat, phoneLng;
 var DEGREE_MAS_RATIO = 0.000000277777778;
 function init() {
+	
+	/** Converts numeric degrees to radians */
+	if (typeof(Number.prototype.toRad) === "undefined") {
+	  Number.prototype.toRad = function() {
+	    return this * Math.PI / 180;
+	  }
+	}
+	
+	//x$(".contact").click(startDemo);
+
 	$label = x$("#current-location");
 	$id = x$("#car-id");
 	$sync = x$("#sync-status");
+	/*
     var button = document.getElementById('button-test');
     var GMButton = new gm.widgets.Button({
 		"label":"Start Demo",
@@ -12,6 +23,29 @@ function init() {
 		 "parentElement":button
 	});
     GMButton.render();
+    */
+}
+function renderContacts () {
+	var contacts = x$(".contact");
+	for (var i = 0; i < contacts.length; i++)
+	{
+		var contactDetail = contacts[i];
+		var dataId = contactDetail.getAttribute('data-id');
+		console.log(dataId);
+	}
+}
+function distance(lat1, lng1, lat2, lng2)
+{
+	var R = 3956.6; // miles
+	var dLat = (lat2-lat1).toRad();
+	var dLng = (lng2-lng1).toRad(); 
+	var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+	        Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) * 
+	        Math.sin(dLng/2) * Math.sin(dLng/2); 
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+	var d = R * c;
+	var value2 = d;
+	return value2;
 }
 
 function sendLocation(lat, lng, id)
@@ -20,10 +54,13 @@ function sendLocation(lat, lng, id)
 	    function(responseObj) {
 	    	var obj = JSON.parse(responseObj);
 	    	if (obj.status)
-	    	{
+	    	{ 
 	    		phoneLat = obj.phoneLat;
 	    		phoneLng = obj.phoneLng;
-	    		$sync.html("polling.. phone lat: " + phoneLat + "phone lng: " + phoneLng);
+	    		if (phoneLat && phoneLng) {
+	    			var dist = distance(myLat, myLng, phoneLat, phoneLng);
+	    		}
+	    		$sync.html("phone lat: " + phoneLat + "phone lng: " + phoneLng + "distance:" + dist);
 	    	} else {
 	    		$sync.html("FAILED");
 	    		//gm.info.clearPosition(watchPositionID);
@@ -63,7 +100,7 @@ function initiateSync(lat, lng, id) {
 					    {
 					        maximumAge: 30000,
 					        timeout: 30000,
-					        frequency: 3000
+					        frequency: 1000
 					    }
 				);
 			},
