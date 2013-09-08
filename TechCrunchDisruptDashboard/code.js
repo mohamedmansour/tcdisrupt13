de0d4d1c6fab0916c180f35d966ade3cbd555c48
Mohamed Mansour <hello@mohamedmansour.com>
2013-09-07 19:25:12 -0700

    Initial code to sync from server to car

---
+++ TechCrunchDisruptDashboard/code.js
@@ -0,0 +1,38 @@
+function init() {
+	var keyboardBtn = new gm.widgets.Button({ label: "Show keyboard",
+    	callBack: showKeyboardEmail,
+        parentElement: document.getElementById('showKeyboard')
+    });
+    keyboardBtn.render();
+  	    
+   getAPI();   	    
+ }
+
+function getAPI() {
+    var $label = x$("#response");
+	gm.comm.webServiceRequest(
+	    function(responseObj) {
+	        console.log('Success: webServiceRequest.  Response: ' + responseObj);
+	        $label.html(JSON.stringify(responseObj));
+	    },
+	    function(responseObj) {
+	        console.log('Failure: webServiceRequest.  Response: ' + responseObj);
+	    },
+	    {
+	      url: "http://tcdisrupt13.azurewebsites.net/api",
+	      method: "GET"
+	    }
+	);
+}
+
+function onDoneCallBack(data) {
+	  console.log('onDone called with -> ' + data.value);
+}
+
+function onCancelCallBack() {
+	console.log('Canceled!');
+}
+
+function showKeyboardEmail() {
+	var keyboard = new gm.widgets.Keyboard({sender: null, language: "en-US", kbType: 1, feedbackMode: 0, Theme:'gmc', placeholder:'Placeholder', onDone: onDoneCallBack, onCancel:onCancelCallBack});
+}
