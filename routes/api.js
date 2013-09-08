21636b8610a05b1d876fa7e948b825eeafe3afcd
Mohamed Mansour <hello@mohamedmansour.com>
2013-09-07 21:40:21 -0700

    Add APIS to sync and notify the car
    These are used to control the vins

--- routes/api.js
+++ routes/api.js
@@ -1,22 +1,79 @@
 exports.attach = function(app) {
-	app.get('/api', function(req, res) {
+	var vins = {};
+
+	app.get('/api/sync', function(req, res) {
 
 		var lat = parseFloat(req.query.lat);
 		var lng = parseFloat(req.query.lng);
+		var vin = req.query.vin;
+
 		var result = null;
-		if (!lat || !lng) {
+		if (!lat || !lng || !vin) {
+			result = {
+				status: false
+			};
+		}
+		else {
+			vins[vin] = {
+				lat: lat,
+				lng: lng
+			};
+			result = {
+				status: true
+			};
+		}
+
+		res.send(result);
+	});	
+
+	app.get('/api/contacts', function(req, res) {
+		res.send({
+			status: false
+		})
+	});
+
+	app.get('/api/notify', function(req, res) {
+		var lat = parseFloat(req.query.lat);
+		var lng = parseFloat(req.query.lng);
+		var vin = req.query.vin;
+		var phone = req.query.phone;
+
+		var result = null;
+		if (!lat || !lng || !vin || !phone) {
 			result = {
 				status: false
 			};
 		}
 		else {
 			result = {
+				status: true
+			};
+
+			// Send Twillio API the phone.
+		}
+
+		res.send(result);
+	});
+
+	app.get('/api/get', function(req, res) {
+		var vin = req.query.vin;
+
+		var result = null;
+		if (!vin) {
+			result = {
+				status: false
+			};
+		}
+		else {
+console.log(vins);
+			var vinData = vins[vin] || { lat: 37.77493, lng: -122.41942 }; 
+			result = {
 				status: true,
-				lat: lat,
-				lng: lng
+				lat: vinData.lat,
+				lng: vinData.lng
 			};
 		}
 
 		res.send(result);
-	});	
+	});
 };
